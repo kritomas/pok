797f5cf5ea58a4be28c679b15721ddc59ec47c1f
kritomas <Unknown>
2025-01-21 21:33:35 +0100

    Added feed entry adding function.

--- atomon/feed.js
+++ atomon/feed.js
@@ -12,10 +12,54 @@ function reset()
 {
 	let raw = "" + readFileSync(FEED_TEMPLATE_LOCATION);
 	let xml = parser.parseFromString(raw);
+
 	xml.getElementsByTagName("id")[0].textContent = "urn:uuid:" + uuidv4();
 	xml.getElementsByTagName("updated")[0].textContent = new Date().toISOString();
+
 	writeFileSync(FEED_LOCATION, serializer.serializeToString(xml));
 }
 
-reset();
-//xml = readFileSync(CONFIG_LOCATION);
+export function addAutomatedEntry(title, content)
+{
+	let raw = "" + readFileSync(FEED_TEMPLATE_LOCATION);
+	let xml = parser.parseFromString(raw);
+
+	let entry = xml.createElement("entry")
+
+	let titleElement = xml.createElement("title");
+	titleElement.textContent = title;
+	entry.appendChild(titleElement);
+
+	let author = xml.createElement("author");
+	let authorName = xml.createElement("name");
+	authorName.textContent = "Pok Atom Feed Monitor";
+	author.appendChild(authorName)
+	entry.appendChild(author);
+
+	let id = xml.createElement("id");
+	id.textContent = "urn:uuid:" + uuidv4();
+	entry.appendChild(id);
+
+	let updated = xml.createElement("updated");
+	updated.textContent = new Date().toISOString();
+	entry.appendChild(updated);
+
+	let contentElement = xml.createElement("content");
+	contentElement.setAttribute("type", "text");
+	contentElement.textContent = content;
+	entry.appendChild(contentElement);
+
+	let feed = xml.getElementsByTagName("feed")[0];
+	feed.appendChild(entry);
+
+	writeFileSync(FEED_LOCATION, serializer.serializeToString(xml));
+}
+
+try
+{
+	let xml = readFileSync(CONFIG_LOCATION);
+}
+catch
+{
+	reset();
+}
